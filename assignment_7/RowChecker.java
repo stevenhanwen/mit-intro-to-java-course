a0ef4f94ec6f735481b3ac648352a8a7ebc775c1
stevenhanwen <stevenhanwen.kang@outlook.com>
2025-03-25 09:42:05 -0400

    added file reader

--- assignment_7/RowChecker.java
+++ assignment_7/RowChecker.java
@@ -5,14 +5,27 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class RowChecker {
-    FileReader fr = new FileReader("Mercury.txt");
-    BufferedReader br = new BufferedReader(fr);
-    String line = null;
 
-    while((line=br.readLine())!=null)
-    {
-        System.out.println(line);
+    public void readFile() {
+        try {
+            FileReader fr = new FileReader("./assignment_7/Mercury.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    br.close();
+    public static void main(String[] args) {
+        RowChecker checker = new RowChecker();
+        checker.readFile();
+    }
 }
